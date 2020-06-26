4429e14dcc027461c5400e2d6ab15f1db80c8493
Tanya Tang <tanya.tang56@gmail.com>
2020-06-26 17:13:44 -0400

    added more routes from NHL API to database

--- api/routes/ExternalAPI.js
+++ api/routes/ExternalAPI.js
@@ -8,16 +8,43 @@ var getTeams = (cb) => {
   });
 };
 
-var getCoaches = (cb) => {
-  var url = config.baseURL + "teams";
+var getPlayersPerTeam = (cb, teamID) => {
+  var url = config.baseURL + "teams/" + teamID + "/roster";
+  request(url, (error, response, body) => {
+    cb(error, response, body, teamID);
+  });
+};
+
+var getPlayer = (cb, playerID) => {
+  var url = config.baseURL + "people/" + playerID;
+  request(url, (error, response, body) => {
+    cb(error, response, body, playerID);
+  });
+};
+
+var getSeasons = (cb) => {
+  var url = config.baseURL + "seasons";
   request(url, (error, response, body) => {
     cb(error, response, body);
   });
 };
 
+var getSeasonWinner = (cb, seasonID) => {
+  var url =
+    config.baseURL +
+    "tournaments/playoffs?expand=round.series,schedule.game.seriesSummary&season=" +
+    seasonID;
+  request(url, (error, response, body) => {
+    cb(error, response, body, seasonID);
+  });
+};
+
 var nhlAPIObject = {
   getTeams: getTeams,
-  getCoaches: getCoaches,
+  getPlayersPerTeam: getPlayersPerTeam,
+  getPlayer: getPlayer,
+  getSeasons: getSeasons,
+  getSeasonWinner: getSeasonWinner,
 };
 
 module.exports = nhlAPIObject;
--- api/routes/Router.js
+++ api/routes/Router.js
@@ -22,7 +22,7 @@ module.exports = (app, express) => {
         dataJSON = JSON.parse(data);
         dataJSON = dataJSON["teams"];
         message = "Teams updated.";
-        dataJSON.forEach((teamJSON) => {
+        dataJSON.map((teamJSON) => {
           pool.query(
             `INSERT INTO teams (team_id, team_name, team_abbv, 
                                 active, year_established, 
@@ -41,21 +41,171 @@ module.exports = (app, express) => {
             }
           );
         });
-        res.send(dataJSON);
-      } else {
-        res.send("NHL API could not be reached.");
       }
+      res.send("Teams updated.");
     });
   });
 
-  api.get("/update_coaches", (req, res) => {
-    util.getCoaches((err, response, date) => {
+  api.get("/update_players_per_team", (req, res) => {
+    pool.query(
+      `SELECT team_id 
+         FROM teams`,
+      (error, result) => {
+        if (!error) {
+          result.rows.map((row) =>
+            util.getPlayersPerTeam((err, response, data, teamID) => {
+              if (!err) {
+                dataJSON = JSON.parse(data);
+                dataJSON = dataJSON["roster"];
+                message = "Players updated for team " + teamID;
+                dataJSON.forEach((playerJSON) => {
+                  pool.query(
+                    `INSERT INTO players (player_id, name, jersey_number, primary_position)
+                          VALUES (${playerJSON["person"]["id"]}::integer,
+                                 '${playerJSON["person"]["fullName"]}'::text, 
+                                 ${playerJSON["jerseyNumber"]}::integer, 
+                                '${playerJSON["position"]["abbreviation"]}'::text)`,
+                    (error, results) => {
+                      if (error) {
+                        console.log(error["detail"]);
+                      } else {
+                        console.log(message);
+                      }
+                    }
+                  );
+                });
+              }
+            }, row["team_id"])
+          );
+        }
+      }
+    );
+    res.send("Players updated per team");
+  });
+
+  api.get("/update_players", (req, res) => {
+    pool.query(
+      `SELECT player_id 
+         FROM players`,
+      (error, result) => {
+        if (!error) {
+          result.rows.map((row) =>
+            util.getPlayer((err, response, data, playerID) => {
+              if (!err) {
+                dataJSON = JSON.parse(data);
+                dataJSON = dataJSON["people"];
+                message = "Player " + playerID + " updated";
+                dataJSON.forEach((playerJSON) => {
+                  feet = playerJSON["height"].substring(0, 1);
+                  inches = playerJSON["height"].substring(
+                    playerJSON["height"].search(" ") + 1,
+                    playerJSON["height"].search('"')
+                  );
+                  height = feet * 0.3048 + inches * 0.0254;
+                  height = height.toFixed(4);
+                  weight = playerJSON["weight"] * 0.453592;
+                  weight = weight.toFixed(4);
+                  pool.query(
+                    `UPDATE players 
+                        SET nationality = '${playerJSON["birthCountry"]}'::text, 
+                            birth_date = '${playerJSON["birthDate"]}'::date, 
+                            current_team_id = ${playerJSON["currentTeam"]["id"]}::integer, 
+                            height = ${height}, 
+                            weight = ${weight}, 
+                            rookie = ${playerJSON["rookie"]}::boolean, 
+                            captain = ${playerJSON["captain"]}::boolean, 
+                            alternate_captain = ${playerJSON["alternateCaptain"]}::boolean, 
+                            shoot_catch = '${playerJSON["shootsCatches"]}' 
+                      WHERE player_id = ${playerID}`,
+                    (error, results) => {
+                      if (error) {
+                        console.log(error["detail"]);
+                      } else {
+                        console.log(message);
+                      }
+                    }
+                  );
+                });
+              }
+            }, row["player_id"])
+          );
+        }
+      }
+    );
+    res.send("Players updated.");
+  });
+
+  api.get("/update_seasons", (req, res) => {
+    util.getSeasons((err, response, data) => {
       if (!err) {
-        res.send();
-      } else {
-        res.send("NHL API could not be reached.");
+        dataJSON = JSON.parse(data);
+        dataJSON = dataJSON["seasons"];
+        dataJSON.map((seasonJSON) => {
+          if (parseInt(seasonJSON["seasonId"]) > 19801981) {
+            pool.query(
+              `INSERT INTO seasons (season_id, regular_season_start_date, 
+                                  regular_season_end_date, season_end_date, 
+                                  num_games)
+                  VALUES (${seasonJSON["seasonId"]}::text, 
+                          '${seasonJSON["regularSeasonStartDate"]}'::date, 
+                          '${seasonJSON["regularSeasonEndDate"]}'::date, 
+                          '${seasonJSON["seasonEndDate"]}'::date, 
+                          ${seasonJSON["numberOfGames"]}::integer)`,
+              (error, results) => {
+                if (error) {
+                  console.log(error["detail"]);
+                }
+              }
+            );
+          }
+        });
       }
     });
+    res.send("Seasons updated.");
+  });
+
+  api.get("/update_season_winners", (req, res) => {
+    pool.query(
+      `SELECT season_id
+         FROM seasons`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getSeasonWinner((err, response, data, seasonID) => {
+              if (!err) {
+                try {
+                  dataJSON = JSON.parse(data);
+                  dataJSON = dataJSON["rounds"].slice(-1)[0];
+                  dataJSON = dataJSON["series"][0]["matchupTeams"];
+                  winner = null;
+                  if (
+                    dataJSON[0]["seriesRecord"]["wins"] >
+                    dataJSON[0]["seriesRecord"]["losses"]
+                  ) {
+                    winner = dataJSON[0]["team"]["id"];
+                  } else {
+                    winner = dataJSON[1]["team"]["id"];
+                  }
+                  pool.query(
+                    `UPDATE seasons 
+                        SET winning_team_id = ${winner} 
+                      WHERE season_id = '${seasonID}'`,
+                    (error1, results1) => {
+                      if (error1) {
+                        console.log(error1["detail"]);
+                      }
+                    }
+                  );
+                } catch {
+                  console.log(seasonID + " not updated.");
+                }
+              }
+            }, row["season_id"]);
+          });
+        }
+      }
+    );
+    res.send("Season winners updated.");
   });
 
   return api;
