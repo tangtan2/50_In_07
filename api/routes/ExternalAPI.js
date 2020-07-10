828ed6f73d693facb93e500463eee25712249226
Tanya Tang <tanya.tang56@gmail.com>
2020-07-10 19:00:01 -0400

    more routes to add historical info to database

--- api/routes/ExternalAPI.js
+++ api/routes/ExternalAPI.js
@@ -46,6 +46,49 @@ var getGamesPerSeason = (cb, seasonID) => {
   });
 };
 
+var getInactivePlayersPerTeamPerSeason = (cb, teamID, seasonID) => {
+  var url =
+    config.baseURL +
+    "teams/" +
+    teamID +
+    "?expand=team.roster&season=" +
+    seasonID;
+  request(url, (error, response, body) => {
+    cb(error, response, body, teamID, seasonID);
+  });
+};
+
+var getPlayerStatsSingleSeason = (cb, playerID, seasonID) => {
+  var url =
+    config.baseURL +
+    "people/" +
+    playerID +
+    "/stats?stats=statsSingleSeason&season=" +
+    seasonID;
+  request(url, (error, response, body) => {
+    cb(error, response, body, playerID, seasonID);
+  });
+};
+
+var getTeamStatsSingleSeason = (cb, teamID, seasonID) => {
+  var url =
+    config.baseURL +
+    "teams/" +
+    teamID +
+    "/stats?stats=statsSingleSeason&season=" +
+    seasonID;
+  request(url, (error, response, body) => {
+    cb(error, response, body, teamID, seasonID);
+  });
+};
+
+var getGames = (cb, gameID) => {
+  var url = config.baseURL + "game/" + gameID + "/linescore?periods";
+  request(url, (error, response, body) => {
+    cb(error, response, body, gameID);
+  });
+};
+
 var nhlAPIObject = {
   getTeams: getTeams,
   getPlayersPerTeam: getPlayersPerTeam,
@@ -53,6 +96,10 @@ var nhlAPIObject = {
   getSeasons: getSeasons,
   getSeasonWinner: getSeasonWinner,
   getGamesPerSeason: getGamesPerSeason,
+  getInactivePlayersPerTeamPerSeason: getInactivePlayersPerTeamPerSeason,
+  getPlayerStatsSingleSeason: getPlayerStatsSingleSeason,
+  getTeamStatsSingleSeason: getTeamStatsSingleSeason,
+  getGames: getGames,
 };
 
 module.exports = nhlAPIObject;
--- api/routes/Router.js
+++ api/routes/Router.js
@@ -94,38 +94,36 @@ module.exports = (app, express) => {
           result.rows.map((row) =>
             util.getPlayer((err, response, data, playerID) => {
               if (!err) {
-                dataJSON = JSON.parse(data);
-                dataJSON = dataJSON["people"];
-                message = "Player " + playerID + " updated";
-                dataJSON.forEach((playerJSON) => {
-                  feet = playerJSON["height"].substring(0, 1);
-                  inches = playerJSON["height"].substring(
-                    playerJSON["height"].search(" ") + 1,
-                    playerJSON["height"].search('"')
-                  );
-                  height = feet * 0.3048 + inches * 0.0254;
-                  height = height.toFixed(4);
-                  weight = playerJSON["weight"] * 0.453592;
-                  weight = weight.toFixed(4);
-                  pool.query(
-                    `UPDATE players 
+                try {
+                  dataJSON = JSON.parse(data);
+                  dataJSON = dataJSON["people"];
+                  dataJSON.forEach((playerJSON) => {
+                    feet = playerJSON["height"].substring(0, 1);
+                    inches = playerJSON["height"].substring(
+                      playerJSON["height"].search(" ") + 1,
+                      playerJSON["height"].search('"')
+                    );
+                    height = feet * 0.3048 + inches * 0.0254;
+                    height = height.toFixed(4);
+                    weight = playerJSON["weight"] * 0.453592;
+                    weight = weight.toFixed(4);
+                    pool.query(
+                      `UPDATE players 
                         SET nationality = '${playerJSON["birthCountry"]}'::text, 
-                            birth_date = '${playerJSON["birthDate"]}'::date, 
-                            current_team_id = ${playerJSON["currentTeam"]["id"]}::integer, 
-                            height = ${height}, 
-                            weight = ${weight}, 
-                            rookie = ${playerJSON["rookie"]}::boolean, 
                             captain = ${playerJSON["captain"]}::boolean, 
                             alternate_captain = ${playerJSON["alternateCaptain"]}::boolean, 
-                            shoot_catch = '${playerJSON["shootsCatches"]}' 
+                            active = '${playerJSON["active"]}'::boolean 
                       WHERE player_id = ${playerID}`,
-                    (error1, results1) => {
-                      if (error) {
-                        console.log(error1["detail"]);
+                      (error1, results1) => {
+                        if (error) {
+                          console.log(error1["detail"]);
+                        }
                       }
-                    }
-                  );
-                });
+                    );
+                  });
+                } catch {
+                  console.log("Player " + playerID + " not updated.");
+                }
               }
             }, row["player_id"])
           );
@@ -225,7 +223,7 @@ module.exports = (app, express) => {
                     dateJSON.map((gameJSON) => {
                       pool.query(
                         `INSERT INTO games (game_id, season_id, category_id, 
-                                            date_time_GMT, away_team_id, 
+                                            date_time, away_team_id, 
                                             home_team_id, away_goals, home_goals) 
                               VALUES (${gameJSON["gamePk"]}::integer, 
                                       '${gameJSON["season"]}'::text, 
@@ -237,14 +235,14 @@ module.exports = (app, express) => {
                                       ${gameJSON["teams"]["home"]["score"]})`,
                         (error1, results1) => {
                           if (error1) {
-                            console.log(error["detail"]);
+                            console.log(error1["detail"]);
                           }
                         }
                       );
                     });
                   });
                 } catch {
-                  console.log(seasonID + " not updated");
+                  console.log(seasonID + " not updated.");
                 }
               }
             }, row["season_id"]);
@@ -253,6 +251,310 @@ module.exports = (app, express) => {
       }
     );
     res.send("Games updated per season.");
+  });
+
+  api.get("/update_inactive_players_per_team_per_season", (req, res) => {
+    pool.query(
+      `SELECT a.team_id, b.season_id
+         FROM teams AS a
+   CROSS JOIN seasons AS b`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getInactivePlayersPerTeamPerSeason(
+              (err, response, data, teamID, seasonID) => {
+                if (!err) {
+                  try {
+                    dataJSON = JSON.parse(data);
+                    dataJSON = dataJSON["teams"][0]["roster"]["roster"];
+                    dataJSON.map((playerJSON) => {
+                      pool.query(
+                        `INSERT INTO players (player_id, name, primary_position, active)
+                              VALUES (${playerJSON["person"]["id"]}::integer, 
+                                      $$${playerJSON["person"]["fullName"]}$$::text, 
+                                      '${playerJSON["position"]["abbreviation"]}'::text, 
+                                      'False')`,
+                        (error1, results1) => {
+                          if (error1) {
+                            console.log(error1["detail"]);
+                          }
+                        }
+                      );
+                    });
+                  } catch {
+                    console.log(
+                      "Players in season " +
+                        seasonID +
+                        " for team " +
+                        teamID +
+                        " not updated."
+                    );
+                  }
+                }
+              },
+              row["team_id"],
+              row["season_id"]
+            );
+          });
+        }
+      }
+    );
+    res.send("Inactive players updated per team per season.");
+  });
+
+  api.get("/update_player_stats_single_season", (req, res) => {
+    pool.query(
+      `SELECT a.player_id, b.season_id 
+         FROM players AS a 
+   CROSS JOIN seasons AS b 
+        WHERE (b.season_id = '19841988' 
+           OR b.season_id = '19831984' 
+           OR b.season_id = '19821983' 
+           OR b.season_id = '19811982') 
+          AND a.primary_position != 'G'`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getPlayerStatsSingleSeason(
+              (err, response, data, playerID, seasonID) => {
+                if (!err) {
+                  try {
+                    dataJSON = JSON.parse(data);
+                    dataJSON = dataJSON["stats"][0]["splits"][0];
+                    pool.query(
+                      `INSERT INTO player_stats_single_season (season_id, player_id, 
+                                   assists, goals, shots, games, penalty_minutes, 
+                                   power_play_goals, power_play_points, short_goals, 
+                                   short_points, game_winning_goals, 
+                                   overtime_goals, plus_minus, points) 
+                            VALUES ('${dataJSON["season"]}'::text, 
+                                    ${playerID}::integer,
+                                    ${dataJSON["stat"]["assists"]}::integer, 
+                                    ${dataJSON["stat"]["goals"]}::integer, 
+                                    ${dataJSON["stat"]["shots"]}::integer, 
+                                    ${dataJSON["stat"]["games"]}::integer, 
+                                    ${dataJSON["stat"]["penaltyMinutes"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlayGoals"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlayPoints"]}::integer, 
+                                    ${dataJSON["stat"]["shortHandedGoals"]}::integer, 
+                                    ${dataJSON["stat"]["shortHandedPoints"]}::integer, 
+                                    ${dataJSON["stat"]["gameWinningGoals"]}::integer, 
+                                    ${dataJSON["stat"]["overTimeGoals"]}::integer, 
+                                    ${dataJSON["stat"]["plusMinus"]}::integer, 
+                                    ${dataJSON["stat"]["points"]}::integer)`,
+                      (error1, results1) => {
+                        if (error1) {
+                          console.log(error1);
+                          console.log(playerID);
+                        }
+                      }
+                    );
+                  } catch {
+                    console.log(
+                      "Player " +
+                        playerID +
+                        " not updated for season " +
+                        seasonID +
+                        "."
+                    );
+                  }
+                }
+              },
+              row["player_id"],
+              row["season_id"]
+            );
+          });
+        }
+      }
+    );
+    res.send("Player stats updated (single season).");
+  });
+
+  api.get("/update_goalie_stats_single_season", (req, res) => {
+    pool.query(
+      `SELECT a.player_id, b.season_id
+         FROM players AS a
+   CROSS JOIN seasons AS b
+        WHERE (b.season_id = '20182019' 
+           OR b.season_id = '20172018') 
+          AND a.primary_position = 'G'`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getPlayerStatsSingleSeason(
+              (err, response, data, playerID, seasonID) => {
+                if (!err) {
+                  try {
+                    dataJSON = JSON.parse(data);
+                    dataJSON = dataJSON["stats"][0]["splits"][0];
+                    pool.query(
+                      `INSERT INTO goalie_stats_single_season (season_id, player_id, 
+                                   overtime, shutouts, wins, losses, saves, 
+                                   power_play_saves, power_play_shots, short_saves, short_shots, 
+                                   even_saves, even_shots, save_percentage, goals_against_average, 
+                                   games, games_started, shots_against, goals_against, time_on_ice) 
+                            VALUES ('${dataJSON["season"]}'::text, 
+                                    ${playerID}::integer,
+                                    ${dataJSON["stat"]["ot"]}::integer, 
+                                    ${dataJSON["stat"]["shutouts"]}::integer, 
+                                    ${dataJSON["stat"]["wins"]}::integer, 
+                                    ${dataJSON["stat"]["losses"]}::integer, 
+                                    ${dataJSON["stat"]["saves"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlaySaves"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlayShots"]}::integer, 
+                                    ${dataJSON["stat"]["shortHandedSaves"]}::integer, 
+                                    ${dataJSON["stat"]["shortHandedShots"]}, 
+                                    ${dataJSON["stat"]["evenSaves"]}::integer, 
+                                    ${dataJSON["stat"]["evenShots"]}::integer, 
+                                    ${dataJSON["stat"]["savePercentage"]}::double precision, 
+                                    ${dataJSON["stat"]["goalAgainstAverage"]}::double precision, 
+                                    ${dataJSON["stat"]["games"]}::integer, 
+                                    ${dataJSON["stat"]["gamesStarted"]}::integer, 
+                                    ${dataJSON["stat"]["shotsAgainst"]}::integer, 
+                                    ${dataJSON["stat"]["goalsAgainst"]}::integer, 
+                                    '${dataJSON["stat"]["timeOnIce"]}'::interval minute)`,
+                      (error1, results1) => {
+                        if (error1) {
+                          console.log(error1);
+                        }
+                      }
+                    );
+                  } catch {
+                    console.log(
+                      "Player " +
+                        playerID +
+                        " not updated for season " +
+                        seasonID +
+                        "."
+                    );
+                  }
+                }
+              },
+              row["player_id"],
+              row["season_id"]
+            );
+          });
+        }
+      }
+    );
+    res.send("Goalie stats updated (single season).");
+  });
+
+  api.get("/update_team_stats_single_season", (req, res) => {
+    pool.query(
+      `SELECT a.team_id, b.season_id
+         FROM teams AS a
+   CROSS JOIN seasons AS b`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getTeamStatsSingleSeason(
+              (err, response, data, teamID, seasonID) => {
+                if (!err) {
+                  try {
+                    dataJSON = JSON.parse(data);
+                    dataJSON = dataJSON["stats"][0]["splits"][0];
+                    pool.query(
+                      `INSERT INTO team_stats_single_season (season_id, team_id, 
+                                   games_played, wins, losses, overtime, points, 
+                                   point_percentage, goals_per_game, goals_against_per_game, 
+                                   ev_gga_ratio, power_play_percentage, power_play_goals, 
+                                   power_play_goals_against, power_play_opportunities, 
+                                   penalty_kill_percentage, shots_per_game, shots_allowed, 
+                                   win_score_first, win_opp_score_first, win_lead_first_per, 
+                                   win_lead_second_per, win_outshoot_opp, win_outshot_by_opp, 
+                                   faceoffs_won, faceoffs_lost, faceoffs_taken, 
+                                   shooting_percentage, save_percentage) 
+                            VALUES ('${seasonID}'::text, 
+                                    ${dataJSON["team"]["id"]}::integer, 
+                                    ${dataJSON["stat"]["gamesPlayed"]}::integer, 
+                                    ${dataJSON["stat"]["wins"]}::integer, 
+                                    ${dataJSON["stat"]["losses"]}::integer, 
+                                    ${dataJSON["stat"]["ot"]}::integer, 
+                                    ${dataJSON["stat"]["pts"]}::integer, 
+                                    ${dataJSON["stat"]["ptPctg"]}, 
+                                    ${dataJSON["stat"]["goalsPerGame"]}, 
+                                    ${dataJSON["stat"]["goalsAgainstPerGame"]}, 
+                                    ${dataJSON["stat"]["evGGARatio"]}, 
+                                    ${dataJSON["stat"]["powerPlayPercentage"]}, 
+                                    ${dataJSON["stat"]["powerPlayGoals"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlayGoalsAgainst"]}::integer, 
+                                    ${dataJSON["stat"]["powerPlayOpportunities"]}::integer, 
+                                    ${dataJSON["stat"]["penaltyKillPercentage"]}, 
+                                    ${dataJSON["stat"]["shotsPerGame"]}, 
+                                    ${dataJSON["stat"]["shotsAllowed"]}, 
+                                    ${dataJSON["stat"]["winScoreFirst"]}, 
+                                    ${dataJSON["stat"]["winOppScoreFirst"]}, 
+                                    ${dataJSON["stat"]["winLeadFirstPer"]}, 
+                                    ${dataJSON["stat"]["winLeadSecondPer"]}, 
+                                    ${dataJSON["stat"]["winOutshootOpp"]}, 
+                                    ${dataJSON["stat"]["winOutshotByOpp"]}, 
+                                    ${dataJSON["stat"]["faceOffsWon"]}::integer, 
+                                    ${dataJSON["stat"]["faceOffsLost"]}::integer, 
+                                    ${dataJSON["stat"]["faceOffsTaken"]}::integer, 
+                                    ${dataJSON["stat"]["shootingPctg"]}, 
+                                    ${dataJSON["stat"]["savePctg"]})`,
+                      (error1, results1) => {
+                        if (error1) {
+                          console.log(error1["detail"]);
+                          console.log(teamID);
+                          console.log(seasonID);
+                        }
+                      }
+                    );
+                  } catch (err) {
+                    console.log(
+                      "Team " + teamID + " not updated for season " + seasonID
+                    );
+                  }
+                }
+              },
+              row["team_id"],
+              row["season_id"]
+            );
+          });
+        }
+      }
+    );
+    res.send("Team stats updated (single season).");
+  });
+
+  api.get("/update_games_aux", (req, res) => {
+    pool.query(
+      `SELECT game_id 
+         FROM games 
+        WHERE settled_in_period_ordinal IS NULL`,
+      (error, results) => {
+        if (!error) {
+          results.rows.map((row) => {
+            util.getGames((err, response, data, gameID) => {
+              if (!err) {
+                try {
+                  dataJSON = JSON.parse(data);
+                  pool.query(
+                    `UPDATE games 
+                        SET settled_in_period_ordinal = '${dataJSON["currentPeriodOrdinal"]}'::text 
+                      WHERE game_id = ${gameID}`,
+                    (error1, results1) => {
+                      if (error1) {
+                        console.log(error1);
+                        console.log(gameID);
+                      } else {
+                        console.log("Game " + gameID + " updated");
+                      }
+                    }
+                  );
+                } catch (err) {
+                  console.log(err);
+                  console.log("Game " + gameID + " not updated.");
+                }
+              }
+            }, row["game_id"]);
+          });
+        }
+      }
+    );
+    res.send("Game auxiliary information updated.");
   });
 
   return api;
