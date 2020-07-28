5f36128414df8f279a28c2441298afee6ac37107
Tanya Tang <tanya.tang56@gmail.com>
2020-07-27 20:14:29 -0400

    completed update active players and add new seasons routes

--- server/src/routes/updatePlayers.ts
+++ server/src/routes/updatePlayers.ts
@@ -2,15 +2,15 @@ import express from "express";
 import { Pool } from "pg";
 import { nhlAPIObject } from "./index";
 
-const getActivePlayers = async (pool: Pool): Promise<Array<any>> => {
+const getActivePlayers = async (pool: Pool): Promise<Array<number>> => {
   return pool
     .query(
-      `SELECT * 
-       FROM players 
-      WHERE active = True`
+      `SELECT player_id 
+         FROM players 
+        WHERE active = True`
     )
     .then((queryRes) => {
-      console.log("Success!");
+      console.log("Success, got all active players from database");
       return queryRes.rows;
     })
     .catch((queryErr) => {
@@ -19,24 +19,41 @@ const getActivePlayers = async (pool: Pool): Promise<Array<any>> => {
     });
 };
 
-const updateDBPlayer = (data: JSON, pool: Pool) => {
+const updatePlayerDB = (data: JSON, pool: Pool) => {
   pool
     .query(
-      `UPDATE players
-          SET ()`
+      `UPDATE players 
+          SET active = ${data["people"][0]["active"]}, 
+              alternateCaptain = ${data["people"][0]["alternateCaptain"]}, 
+              captain = ${data["people"][0]["captain"]}, 
+              rookie = ${data["people"][0]["rookie"]}, 
+              current_team_id = ${data["currentTeam"][0]["id"]}`
     )
-    .then((queryRes) => {
-      console.log("Success!");
+    .then((_) => {
+      console.log("Success, updated existing player in database");
     })
     .catch((queryErr) => {
       console.log(queryErr);
     });
 };
 
+const updatePlayerFromNHLAPI = (playerID: number, pool: Pool) => {
+  nhlAPIObject.getPlayerByID({ playerID: playerID }).then((playerJSON) => {
+    updatePlayerDB(playerJSON, pool);
+  });
+};
+
 export default (pool: Pool) => {
   return (_: express.Request, res: express.Response) => {
-    nhlAPIObject.getTeams().then((json) => {
-      res.send(json);
-    });
+    getActivePlayers(pool)
+      .then((playersFromDB) => {
+        playersFromDB.map((playerRow) => {
+          updatePlayerFromNHLAPI(playerRow["player_id"], pool);
+        });
+      })
+      .then(() => res.send("Active players updated"))
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
--- server/src/routes/updateSeasons.ts
+++ server/src/routes/updateSeasons.ts
@@ -2,10 +2,57 @@ import express from "express";
 import { Pool } from "pg";
 import { nhlAPIObject } from "./index";
 
+const getMostRecentSeason = async (pool: Pool): Promise<string> => {
+  return pool
+    .query(
+      `SELECT season_id 
+         FROM seasons 
+     ORDER BY regular_season_start_date DESC 
+        LIMIT 1`
+    )
+    .then((queryRes) => {
+      console.log("Success, got most recent season from database");
+      return queryRes.rows[0];
+    })
+    .catch((queryErr) => {
+      console.log(queryErr);
+      return undefined;
+    });
+};
+
+const addSeasonDB = (data: JSON, pool: Pool) => {
+  pool
+    .query(
+      `INSERT INTO seasons ()
+            VALUES ()`
+    )
+    .then((_) => {
+      console.log("Success, added new season to database");
+    })
+    .catch((queryErr) => {
+      console.log(queryErr);
+    });
+};
+
+const updateSeasonFromNHLAPI = (seasonID: string, pool: Pool) => {
+  nhlAPIObject.getSeasons().then((allSeasonJSONs) => {
+    allSeasonJSONs["seasons"].reduceRight((_: JSON, seasonJSON: JSON) => {
+      if (seasonJSON["seasonID"] > seasonID) {
+        addSeasonDB(seasonJSON, pool);
+      }
+    });
+  });
+};
+
 export default (pool: Pool) => {
   return function (_: express.Request, res: express.Response) {
-    nhlAPIObject.getSeasons().then((json) => {
-      res.send(json);
-    });
+    getMostRecentSeason(pool)
+      .then((seasonID) => {
+        updateSeasonFromNHLAPI(seasonID, pool);
+      })
+      .then(() => res.send("New seasons added"))
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
