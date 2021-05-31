b9fdd8b3ba85349f6da3267d34f9a4173a08c632
Tanya Tang <tanya.tang56@gmail.com>
2021-05-31 09:28:49 -0400

    defined routes for api

--- python/src/api/api.py
+++ python/src/api/api.py
@@ -1,10 +1,46 @@
-from flask import Flask, request, jsonify
+import numpy as np
+from flask import Flask, request
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model/model.pkl', 'rb'))
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return 'Make a prediction!'
+
+
+@app.route('/classification_svm', methods=['POST'])
+def classification_svm():
+    feature_values = [np.array(request.form['feature_values'])]
+    prediction = classification_svm.predict(feature_values)
+    return prediction
+
+
+@app.route('/classification_rf', methods=['POST'])
+def classification_rf():
+    feature_values = [np.array(request.form['feature_values'])]
+    prediction = classification_rf.predict(feature_values)
+    return prediction
+
+
+@app.route('/regression_en', methods=['POST'])
+def regression_en():
+    feature_values = [np.array(request.form['feature_values'])]
+    prediction = regression_en.predict(feature_values)
+    return prediction
+
+
+@app.route('/regression_mlp', methods=['POST'])
+def regression_mlp():
+    feature_values = [np.array(request.form['feature_values'])]
+    prediction = regression_mlp.predict(feature_values)
+    return prediction
+
+
+if __name__ == '__main__':
+    classification_svm = pickle.load(open('models/classification_pipeline_svm.pkl', 'rb'))
+    classification_rf = pickle.load(open('models/classification_pipeline_rf.pkl', 'rb'))
+    regression_en = pickle.load(open('models/regression_pipeline_en.pkl', 'rb'))
+    regression_mlp = pickle.load(open('models/regression_pipeline_mlp.pkl', 'rb'))
+    app.run(debug=True)
